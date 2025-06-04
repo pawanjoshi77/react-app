2d4b11ab666c2080335181085995854b662080e1
Pawan Joshi <pawanjoshi77@gmail.com>
2025-06-04 19:00:13 -0400

    Created expense list using a state hook.

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,38 @@ import Alert from './components/Alert';
 import Button from './components/Button/Button';
 import ListGroup from './components/ListGroup/';
 import './App.css'
-import {BsFillCalendarFill} from 'react-icons/bs';
+import { BsFillCalendarFill } from 'react-icons/bs';
 import Like from './components/Like';
 import Form from './components/Form';
+import ExpenseList from './components/expense-tracker/components/ExpenseList';
 
 
 function App() {
-  let items = [
-    'New York',
-    'San Francisco',
-    'Tokyo',
-    'London',
-    'Paris'
-]; 
+  //   let items = [
+  //     'New York',
+  //     'San Francisco',
+  //     'Tokyo',
+  //     'London',
+  //     'Paris'
+  // ]; 
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: 'aaa', amount: 10, category: 'Utilities' },
+    { id: 2, description: 'bbb', amount: 10, category: 'Utilities' },
+    { id: 3, description: 'ccc', amount: 10, category: 'Utilities' },
+    { id: 4, description: 'ddd', amount: 10, category: 'Utilities' }
+  ]);
+
+  // const expenses = [
+  //   {id: 1, description: 'aaa', amount:10, category: 'Utilities'},
+  //   {id: 2, description: 'bbb', amount:10, category: 'Utilities'},
+  //   {id: 3, description: 'ccc', amount:10, category: 'Utilities'},
+  //   {id: 4, description: 'ddd', amount:10, category: 'Utilities'}
+  // ]
+
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // }
   // let buttons = [
   //   { text: 'Primary', color: 'blue' },
   //   { text: 'Secondary', color: 'grey' },
@@ -31,20 +46,22 @@ function App() {
   //   { text: 'Dark', color: 'black' },
   // ];
 
-  
-  
-  return (
-    <div>  
 
-      <Form />
-      
+
+  return (
+    <div>
+
+      <ExpenseList expenses={expenses} onDelete={(id) => setExpenses(expenses.filter(e => e.id !== id))} />
+
+      {/* <Form /> */}
+
       {/* <Alert>
         Hello <span>World</span>
       </Alert> */}
       {/* <ListGroup items= {items} heading="Cities" onSelectItem={handleSelectItem} /> */}
 
     </div>
-  
+
     // <BsFillCalendarFill color='red' size={40} />
     // <div>
 
@@ -53,17 +70,17 @@ function App() {
     // </div>
     //<Like onClick={()=> console.log('clicked')}></Like>
 
-    
-    
+
+
   )
-    {/*
+  {/*
     <Alert>
       Hello <span style={{color: 'red'}}>World!</span>
   </Alert>*/}
 
 
-    {/*<Button text='Secondary' color='grey'></Button> */}
-    {/* {buttons.map((button, index) => (
+  {/*<Button text='Secondary' color='grey'></Button> */ }
+  {/* {buttons.map((button, index) => (
       <button key={index} 
       style={{ 
         backgroundColor: button.color, 
@@ -76,10 +93,10 @@ function App() {
       </button>
     ))} */}
 
-    {/* {<Button text='Primary' customStyle={{padding: '10px 20px', margin: '5px', backgroundColor:'blue', color:'white'}} />} */}
-    {/*<Button text='Secondary' customStyle={{padding: '10px 20px', margin: '5px', backgroundColor:'grey', color:'white'}} />*/}
+  {/* {<Button text='Primary' customStyle={{padding: '10px 20px', margin: '5px', backgroundColor:'blue', color:'white'}} />} */ }
+  {/*<Button text='Secondary' customStyle={{padding: '10px 20px', margin: '5px', backgroundColor:'grey', color:'white'}} />*/ }
 
 }
-  
+
 
 export default App;
